df0c4b7a50f611c48f1c3be3c67001f3b2ad4658
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-12 22:46:51 -0500

    Sets company data model

--- Phonebook/Models/Company.swift
+++ Phonebook/Models/Company.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Company: Identifiable, Decodable {
+    let id = UUID()
+    var companyName: String
+    var departments: [Department]
+    
+    enum CodingKeys: String, CodingKey {
+        case companyName = "company_name"
+        case departments
+    }
+}
+
+struct Companies: Decodable {
+    var companies: [Company]
+}
