ff495e83b59b1748ae9d94b9be0198bf126ac21a
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-12 22:48:17 -0500

    Implenets JSON parsing for data file

--- Phonebook/Service/DataService.swift
+++ Phonebook/Service/DataService.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct DataService {
+    
+    func getData() -> [Company] {
+        
+        if let url = Bundle.main.url(forResource: "PBData", withExtension: "json") {
+            
+            do {
+                let data = try Data(contentsOf: url)
+                
+                let decoder = JSONDecoder()
+                
+                do {
+                    let companies = try decoder.decode(Companies.self, from: data)
+                    return companies.companies
+                } catch {
+                    print("Error parsing JSON: \(error)")
+                }
+                
+            } catch {
+                print("Error reading file: \(error)")
+            }
+        }
+        
+        return [Company]()
+        
+        
+    }
+}
