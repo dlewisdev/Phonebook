e303a9095180f28dd1ebf02865eb2ca72169ab70
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-12 21:39:37 -0500

    Initial Commit

---
+++ Phonebook/PhonebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhonebookApp.swift
+//  Phonebook
+//
+//  Created by Danielle Lewis on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PhonebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
