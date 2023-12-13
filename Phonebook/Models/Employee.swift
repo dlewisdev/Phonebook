789e3ee813ac2802613772281f6f3dae96fdc9ab
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-12 22:47:18 -0500

    Sets employee data model

--- Phonebook/Models/Employee.swift
+++ Phonebook/Models/Employee.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct Employee: Identifiable, Decodable {
+    let id = UUID()
+    var name: String
+    var position: String
+    var email: String
+    var phone: String
+}
